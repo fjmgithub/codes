7f5d854a1d25d00cba4d6dbe7b0a03231a7db5aa
UCS_fangjm <@10.108.143.19>
2021-12-06 17:43:24 +0800

    支付模块

--- cloud2020/cloud-provider-payment8001/src/main/java/com/tedu/cloud/PaymentMain8001.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/tedu/cloud/PaymentMain8001.java
@@ -1,10 +1,16 @@
 package com.tedu.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Author: FJM
  * @Date 2021年12月06日 16:38
  * @Description: 启动类
  */
+@SpringBootApplication
 public class PaymentMain8001 {
-
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
 }
