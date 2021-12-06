752fb96d5e96f46749dcf20a2633ffc5ff2b139d
UCS_fangjm <@10.108.143.19>
2021-12-06 17:40:46 +0800

    支付模块

---
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/tedu/cloud/PaymentMain8001.java
@@ -0,0 +1,10 @@
+package com.tedu.cloud;
+
+/**
+ * @Author: FJM
+ * @Date 2021年12月06日 16:38
+ * @Description: 启动类
+ */
+public class PaymentMain8001 {
+
+}
---
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/tedu/cloud/controller/PaymentController.java
@@ -0,0 +1,9 @@
+package com.tedu.cloud.controller;
+
+/**
+ * @Author: FJM
+ * @Date 2021年12月06日 16:37
+ * @Description: 支付模块
+ */
+public class PaymentController {
+}
