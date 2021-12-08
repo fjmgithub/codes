e3966c1129133d038b1414aa5f9b29125b9a2da6
UCS_fangjm <@10.108.143.19>
2021-12-08 14:28:56 +0800

    工具类

---
+++ cloud2020/cloud-api-commons/src/main/java/com/tedu/entities/CommonResult.java
@@ -0,0 +1,23 @@
+package com.tedu.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author: FJM
+ * @Date 2021年12月08日 14:26
+ * @Description: JSON对象
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonResult<T>{
+    private Integer code;
+    private String message;
+    private T data;
+
+    public CommonResult(Integer code, String message){
+        this(code, message, null);
+    }
+}
---
+++ cloud2020/cloud-api-commons/src/main/java/com/tedu/entities/Payment.java
@@ -0,0 +1,20 @@
+package com.tedu.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @Author: FJM
+ * @Date 2021年12月08日 14:25
+ * @Description: 支付信息
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+    private Long id;
+    private String serial;
+}
