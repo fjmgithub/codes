96a86b0905cfc8c5c88e93d33cb1273b8953f534
UCS_fangjm <@10.108.143.19>
2021-12-09 17:30:00 +0800

    eureka集群

--- cloud2020/cloud-consumer-order8090/src/main/java/com/tedu/cloud/controller/OrderController.java
+++ cloud2020/cloud-consumer-order8090/src/main/java/com/tedu/cloud/controller/OrderController.java
@@ -7,9 +7,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -21,22 +19,23 @@ import javax.annotation.Resource;
  */
 @RestController
 public class OrderController {
-    public static final String GATEWAY = "http://localhost:8001";
-    public static final String URI = "/payment";
+//    public static final String GATEWAY = "http://localhost:8001"
+    public static final String GATEWAY = "http://CLOUD-PAYMENT-SERVICE";
+    public static final String URI = "/provider";
 
     @Resource
     private RestTemplate restTemplate;
 
-    @GetMapping("/consumer/addPayment")
-    public CommonResult<Payment> addPayment(Payment payment){
-        String url = GATEWAY+URI+"addPayment";
-        return doRequestService(payment,url);
+    @PostMapping("/consumer/addPayment")
+    public CommonResult<Payment> addPayment(@RequestBody Payment payment){
+        String url = GATEWAY+URI+"/addPayment";
+        return doRequestService(payment,url,HttpMethod.POST);
     }
 
     @GetMapping("/consumer/getPayment/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
-        String url = GATEWAY+URI+"getPayment"+id;
-        return doRequestService(new Payment(),url);
+        String url = GATEWAY+URI+"/getPayment/"+id;
+        return doRequestService(new Payment(),url,HttpMethod.GET);
     }
 
     /**
@@ -46,11 +45,11 @@ public class OrderController {
      * @Param
      * @Return
      */
-    private <T> CommonResult<T> doRequestService(T t, String url) {
+    private <T> CommonResult<T> doRequestService(T t, String url,HttpMethod httpMethod) {
         HttpHeaders headers = new HttpHeaders();
         HttpEntity<T> httpEntity = new HttpEntity<>(t, headers);
         ParameterizedTypeReference<CommonResult<T>> typeRef =  new ParameterizedTypeReference<CommonResult<T>>(){};
-        ResponseEntity<CommonResult<T>> response = restTemplate.exchange(url, HttpMethod.POST,httpEntity, typeRef);
+        ResponseEntity<CommonResult<T>> response = restTemplate.exchange(url, httpMethod,httpEntity, typeRef);
         return response.getBody();
     }
 
