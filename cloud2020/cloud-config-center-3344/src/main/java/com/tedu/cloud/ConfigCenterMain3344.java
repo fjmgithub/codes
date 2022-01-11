5635f321f4792143a01db099af6bd93e2ff69e61
UCS_fangjm <@10.108.143.19>
2022-01-11 17:32:57 +0800

    spring-cloud-config

---
+++ cloud2020/cloud-config-center-3344/src/main/java/com/tedu/cloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.tedu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: FJM
+ * @Date 2022年01月11日 16:46
+ * @Description: 启动类
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
