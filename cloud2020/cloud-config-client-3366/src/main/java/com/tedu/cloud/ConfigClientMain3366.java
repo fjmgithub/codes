3f317f127d352d3b198985bed214757f4227fca7
UCS_fangjm <@10.108.143.19>
2022-01-21 16:56:30 +0800

    config配置中心客户端

---
+++ cloud2020/cloud-config-client-3366/src/main/java/com/tedu/cloud/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package com.tedu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: FJM
+ * @Date 2022年01月21日 13:56
+ * @Description: 启动类
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
